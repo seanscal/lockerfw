42bcbab5c7fd26018e7f995fa0643311b3e25604
Sean Holmes <doc359@gmail.com>
2016-04-03 21:10:36 -0400

    Update firmware.py

--- firmware.py
+++ firmware.py
@@ -35,7 +35,7 @@ app.logger.info("Started backend engine.")
 
 r_server = redis.Redis('localhost')
 def make_celery(app):
-    celery = Celery(app.import_name, broker =app.config['CELERY_BROKER_URL'], include['firmware',])
+    celery = Celery(app.import_name, broker =app.config['CELERY_BROKER_URL'])
     celery.conf.update(app.config)
     TaskBase = celery.Task
     class ContextTask(TaskBase):
